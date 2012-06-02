9a17c3506359fdadcc4d6134f36b93de3770b8ec
Peter Foster <petefoster0@gmail.com>
2012-06-02 18:59:26 +0100

    base map

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
-	res.render('index.ejs');
+	res.render('index.ejs')
+});
+
+app.get('/test', function(req, res) {
+	console.log('	')
 });
 
 app.listen(3000);
