e6d2a7fef0910c26565b085a0d1a4b1661a84efe
Ben Evans <ben@bensbit.co.uk>
2012-06-02 17:55:59 +0100

    latLong (API Route) Working

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
-express = require('express');
-app = express.createServer();
+var express = require('express'),
+app = express.createServer(),
+mysql      = require('mysql');
+
+var conn = mysql.createConnection({
+	host     : 'localhost',
+	user     : 'root',
+	password : 'root',
+	socketPath : '/var/mysql/mysql.sock',
+	database: 'RHoK_NDWI'
+});
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
+
+
+conn.connect();
+
 
 app.get('/', function(req, res) {
 	res.render('index.ejs');
@@ -10,6 +23,18 @@ app.get('/', function(req, res) {
 
 app.get('/api', function(req, res) {
 	res.render('api');
-})
+});
+
+app.get('/api/latLong/:lat/:long', function(req, res){
+
+	var latitude = req.params.lat,//'51.9947'
+		longitude = req.params.long;//'-1.4976'
+
+	conn.query('SELECT * FROM Data WHERE `longitude` = "' + longitude + '" AND `latitude` = "' + latitude + '"', function(err, rows, fields) {
+		if (err) throw err;
+			res.send(rows);
+
+	});
+});
 
 app.listen(3000);
